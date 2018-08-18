7df50ac5598bdf9d3e954bbb723efa13610507c5
Janaki Syam <jksyam@gmail.com>
2018-08-18 22:27:47 +0530

    Crossing Ball Solution

---
+++ PSet-0005/sketch.js
@@ -0,0 +1,21 @@
+function setup()
+{
+  createCanvas(600,400);
+  noFill();
+  strokeWeight(2);
+}
+var x=1, speed=5;
+
+function draw()
+{
+    //translate(0,200);
+    clear();
+
+   ellipse(x,200,100,100);
+   ellipse(600-x,200,100,100);
+   x = x + speed;
+   if(x<0 || x>600)
+   {
+     speed = speed - 1;
+   }
+}
